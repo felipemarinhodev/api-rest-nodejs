539983026a8d7a6c9749113f228c33a4020f957e
felipemarinhodev <felipmarinho@gmail.com>
2023-02-08 21:05:21 -0800

    refactoring

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -1,21 +1,22 @@
-import { afterAll, beforeAll, expect, test } from 'vitest'
-import { app } from '../app'
 import request from 'supertest'
-
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('should user can create a new transaction', async () => {
-  const response = await request(app.server).post('/transactions').send({
-    title: 'New transaction',
-    amount: 5000,
-    type: 'credit',
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '../app'
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
   })
 
-  expect(response.statusCode).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
