6cb833334478238622b80c28d71f315287e27c2b
felipemarinhodev <felipmarinho@gmail.com>
2023-02-08 21:43:07 -0800

    refactoring test

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -17,24 +17,28 @@ describe('Transactions routes', () => {
   })
 
   it('should be able to create a new transaction', async () => {
-    const response = await request(app.server).post('/transactions').send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-
+    const { createTransactionResponse: response } =
+      await createNewTransactionAndReturnCookiesAndTransactionResponse()
     expect(response.statusCode).toEqual(201)
   })
   it('should be able to list all transactions', async () => {
-    const createTransactionResponse = await request(app.server)
-      .post('/transactions')
-      .send({
+    const { cookies } =
+      await createNewTransactionAndReturnCookiesAndTransactionResponse()
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
         title: 'New transaction',
         amount: 5000,
-        type: 'credit',
-      })
-
-    const cookies = createTransactionResponse.get('Set-Cookie')
+      }),
+    ])
+  })
+  it('should be able to get a specific transactions', async () => {
+    const { cookies } =
+      await createNewTransactionAndReturnCookiesAndTransactionResponse()
     const listTransactionsResponse = await request(app.server)
       .get('/transactions')
       .set('Cookie', cookies)
@@ -48,3 +52,15 @@ describe('Transactions routes', () => {
     ])
   })
 })
+async function createNewTransactionAndReturnCookiesAndTransactionResponse() {
+  const createTransactionResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+  const cookies = createTransactionResponse.get('Set-Cookie')
+  return { cookies, createTransactionResponse }
+}
